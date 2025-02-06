63bf2bd70af25f3bd3c0a0758a871ea7c3fe7f57
Henrik John <l.john@erasmusmc.nl>
2025-02-06 17:06:51 +0100

    Test model

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,20 +2,19 @@
 from context.model import PoincareEmbedding
 from torch.utils.data import DataLoader, BatchSampler, RandomSampler
 from hypll.manifolds.poincare_ball import Curvature, PoincareBall
+import torch
 
-def test_model(dummy_dataset):
-    batch_size = 64
+def test_root_concept_at_origin(dummy_dataset):
     embedding_dim = 3
     curvature = 1.0
-
-    dataloader = DataLoader(dummy_dataset, sampler=BatchSampler(sampler=RandomSampler(dummy_dataset), batch_size=batch_size,
-                                                          drop_last=False))
-
+    root_concept_index = 0
     poincare_ball = PoincareBall(c=Curvature(curvature))
-
     model = PoincareEmbedding(
         num_embeddings=dummy_dataset.num_nodes,
         embedding_dim=embedding_dim,
         manifold=poincare_ball,
-        root_concept_index=0
+        root_concept_index=root_concept_index
     )
+    root_concept = model.weight.data[model.root_concept_index]
+    zero_tensor = torch.zeros(embedding_dim, device=root_concept.device, dtype=root_concept.dtype)
+    assert torch.equal(root_concept, zero_tensor), "Root concept is not a zero tensor."
